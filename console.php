1843b2c41f3f2930dcefdded905285953b67d441
John Pancoast <jpancoast@undergroundelephant.com>
2014-03-30 17:20:11 -0700

    initial configuration

--- console.php
+++ console.php
@@ -2,9 +2,9 @@
 
 require 'vendor/autoload.php';
 
-use Shideon\Tasker\Command\TaskerCommand;
+use Shideon\Tasker as Tasker;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
-$application->add(new TaskerCommand);
+$application->add(new Tasker\Command);
 $application->run();
--- src/Configuration.php
+++ src/Configuration.php
@@ -1,19 +1,66 @@
 <?php
+/**
+ * Tasker configuration
+ *
+ * @copyright (c) 2014 John Pancoast
+ * @author John Pancoast <shideon@gmail.com>
+ * @license MIT
+ */
 
-namespace Shideon\Tasker\Configuration
+namespace Shideon\Tasker;
 
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
+/**
+ * Tasker configuration
+ *
+ * For details on architecture, see docs for symfony's console component
+ * at http://symfony.com/doc/current/components/console/introduction.html.
+ *
+ * @author John Pancoast <shideon@gmail.com>
+ */
 class Configuration implements ConfigurationInterface
 {
+    /**
+     * {@inheritDoc}
+     */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('shideon_tasker');
 
-        // ... add node definitions to the root of the tree
+        $rootNode
+            ->children()
+                ->append($this->appendTasks())
+            ->end();
 
         return $treeBuilder;
+    }
+
+    public function appendTasks()
+    {
+        $builder = new TreeBuilder();
+        $node = $builder->root('tasks');
+
+        $node
+            ->isRequired()
+            ->requiresAtLeastOneElement()
+            ->useAttributeAsKey('task')
+            ->prototype('array')
+                ->children()
+                    ->scalarNode('name')->cannotBeEmpty()->end()
+                    ->scalarNode('time')->cannotBeEmpty()->end()
+                    ->scalarNode('class')->cannotBeEmpty()->end()
+                    ->arrayNode('command')
+                        ->cannotBeEmpty()
+                        ->children()
+                            ->scalarNode('name')->isRequired()->cannotBeEmpty()->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $node;
     }
 }
