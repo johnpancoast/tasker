c505a6c8e979dd15206a4cc2ff0a33d605a2b542
John Pancoast <jpancoast@undergroundelephant.com>
2014-03-30 22:37:28 -0700

    added task validation exception

---
+++ src/Exception/TaskValidationException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Task validation exception
+ *
+ * @copyright (c) 2014 John Pancoast
+ * @author John Pancoast <shideon@gmail.com>
+ * @license MIT
+ */
+
+namespace Shideon\Tasker\Exception;
+
+/**
+ * Task validation exception
+ *
+ * @author John Pancoast <shideon@gmail.com>
+ */
+class TaskValidationException extends \Exception {}
